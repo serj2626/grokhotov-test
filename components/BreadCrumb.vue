c68a09b7bcf5e1ee59a7d164689916aa3d935d61
serj2626 <serj2626@mail.ru>
2025-01-19 19:11:51 +0300

    feat: add global styles for component Header nad BreadCrumbs

--- components/BreadCrumb.vue
+++ components/BreadCrumb.vue
@@ -26,33 +26,3 @@
     </div>
   </div>
 </template>
-
-<style lang="scss" scoped>
-/* BREADCRUMBS */
-.breadcrumb_margin {
-  margin-block: 30px 60px;
-}
-
-.breadcrumb__list {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-}
-
-.breadcrumb__item {
-  list-style: none;
-}
-.breadcrumb__link {
-  text-decoration: none;
-  font-size: 14px;
-  line-height: 150%;
-}
-
-.breadcrumb__link_home {
-  color: #33374e;
-}
-
-.breadcrumb__link_basket {
-  color: #797b86;
-}
-</style>
