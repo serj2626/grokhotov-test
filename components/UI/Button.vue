ec12361cdaa2c90e39b0bae85ad8909eefe06733
serj2626 <serj2626@mail.ru>
2025-01-20 01:09:48 +0300

    feat: new ui components

---
+++ components/UI/Button.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+interface IProps {
+  type: "button" | "submit" | "reset";
+  color: "blue" | "white";
+  size: string;
+}
+</script>
+
+<template>
+  <button class="btn"><slot /></button>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  display: block;
+  border-radius: 4px;
+  font-size: 16px;
+  color: white;
+  background-color: #0069b4;
+  padding-block: 15px;
+  border: none;
+  width: 100%;
+  margin-top: 20px;
+}
+</style>
