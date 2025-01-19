c13bf46020891b2f808c242e5cd171ddc07e2e49
serj2626 <serj2626@mail.ru>
2025-01-20 00:27:04 +0300

    feat: new component for all views products section

---
+++ components/Products/List.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup></script>
+
+<template>
+  <section class="products">
+    <div class="container products__container">
+      <h2 class="products__title">Просмотренные товары</h2>
+      <div class="products__list">
+        <ProductsDetail />
+        <ProductsDetail />
+        <ProductsDetail />
+        <ProductsDetail />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.products__title {
+  color: var(--color-black);
+  font-size: 30px;
+  font-weight: 600;
+  line-height: 120.5%;
+  margin-bottom: 60px;
+}
+.products__list {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+}
+</style>
