78d2cebe0eac4665d6d6f0571b392f5cd26d4e01
serj2626 <serj2626@mail.ru>
2025-01-20 00:33:02 +0300

    refactor: del other style classes

--- components/Products/List.vue
+++ components/Products/List.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <section class="products">
+  <section class="products products_margin">
     <div class="container products__container">
       <h2 class="products__title">Просмотренные товары</h2>
       <div class="products__list">
@@ -13,18 +13,3 @@
     </div>
   </section>
 </template>
-
-<style lang="scss" scoped>
-.products__title {
-  color: var(--color-black);
-  font-size: 30px;
-  font-weight: 600;
-  line-height: 120.5%;
-  margin-bottom: 60px;
-}
-.products__list {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 20px;
-}
-</style>
