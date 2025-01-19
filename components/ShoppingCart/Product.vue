3e67b1e13c0862782d6a18dacf8a9b0d93cceaea
serj2626 <serj2626@mail.ru>
2025-01-20 02:45:53 +0300

    feat: created article for detail product in basket

---
+++ components/ShoppingCart/Product.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup></script>
+
+<template>
+  <!-- saas -->
+  <article class="product">
+    <img
+      class="product__img"
+      src="~/assets/img/products/one.jpg"
+      alt="product"
+      loading="lazy"
+    />
+    <div class="product__info">
+      <h3 class="product__title">Вытяжное устройство G2H</h3>
+      <p class="product__desc">
+        12-72/168 м3/ч / гидрорегулируемый расход /<br />
+        от датчика присутствия
+      </p>
+      <span class="product__article">Артикул: G2H1065</span>
+    </div>
+    <div class="product__actions">
+      <button class="product__btn">-</button>
+      <button class="product__btn">0</button>
+      <button class="product__btn">+</button>
+    </div>
+    <strong class="product__sum">12 644 ₽</strong>
+    <button class="product__close">&times</button>
+  </article>
+</template>
