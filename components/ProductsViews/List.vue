3f332f044ec8e22d380d2984452bb4c74d3612d6
serj2626 <serj2626@mail.ru>
2025-01-20 21:23:53 +0300

    feat: add code

---
+++ components/ProductsViews/List.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { viewsProducts } from "~/types/product_views.data";
+</script>
+
+<template>
+  <section class="products products_margin">
+    <div class="container products__container">
+      <h2 class="products__title">Просмотренные товары</h2>
+      <div class="products__list">
+        <ProductsViewsDetail
+          v-for="(product, index) in viewsProducts"
+          :key="index"
+          :product="product"
+        />
+      </div>
+    </div>
+  </section>
+</template>
