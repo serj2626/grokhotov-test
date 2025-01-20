2abe19758ff4c911d548deb184fe6d01a6a930ff
serj2626 <serj2626@mail.ru>
2025-01-20 19:20:49 +0300

    feat: add func by del all products

--- store/products.ts
+++ store/products.ts
@@ -1,5 +1,5 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue';
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export interface IProduct {
   id: number;
@@ -12,13 +12,13 @@ export interface IProduct {
   currency: string;
 }
 
-export const useProductsStore = defineStore('products', () => {
-
+export const useProductsStore = defineStore("products", () => {
   const products = ref<IProduct[]>([
     {
       id: 1,
       title: "Вытяжное устройство G2H",
-      description: "<span>12-72/168 м3/ч / гидрорегулируемый расход <br>от датчика присутствия</span>",
+      description:
+        "<span>12-72/168 м3/ч / гидрорегулируемый расход <br>от датчика присутствия</span>",
       image: "~/assets/img/products/one.jpg",
       article: "Артикул: G2H1065",
       count: 1,
@@ -28,7 +28,8 @@ export const useProductsStore = defineStore('products', () => {
     {
       id: 2,
       title: "Вытяжное устройство BXC",
-      description: "<span>12-72/168 м3/ч / гидрорегулируемый расход <br>от датчика присутствия</span>",
+      description:
+        "<span>12-72/168 м3/ч / гидрорегулируемый расход <br>от датчика присутствия</span>",
       image: "~/assets/img/products/zero.jpg",
       article: "Артикул: G2H10655",
       count: 2,
@@ -38,55 +39,69 @@ export const useProductsStore = defineStore('products', () => {
     {
       id: 3,
       title: "Вытяжное устройство GHN",
-      description: "<span>12-72/168 м3/ч / гидрорегулируемый расход <br>от датчика присутствия</span>",
+      description:
+        "<span>12-72/168 м3/ч / гидрорегулируемый расход <br>от датчика присутствия</span>",
       image: "~/assets/img/products/two.jpg",
       article: "Артикул: G2H1065",
       count: 1,
       price: 12644,
       currency: "₽",
-    }
-  ])
+    },
+  ]);
 
   const allProducts = computed(() => {
-    return products.value
-  })
-
+    return products.value;
+  });
 
   const totalSum = computed(() => {
     return products.value.reduce((acc, product) => {
-      return acc + product.price * product.count
-    }, 0)
-  })
+      return acc + product.price * product.count;
+    }, 0);
+  });
 
   const totalCount = computed(() => {
     return products.value.reduce((acc, product) => {
-      return acc + product.count
-    }, 0)
-  })
+      return acc + product.count;
+    }, 0);
+  });
 
   const incrementCount = (index: number) => {
     return products.value.map((product, i) => {
       if (i === index) {
-        product.count++
+        product.count++;
       }
-      return product
-    })
-  }
+      return product;
+    });
+  };
 
   const decrementCount = (index: number) => {
     return products.value.map((product, i) => {
       if (i === index && product.count > 1) {
-        product.count--
+        product.count--;
       }
-      return product
-    })
-  }
+      return product;
+    });
+  };
 
-  const checked = ref(true)
-  
+  const delAllProducts = () => {
+    products.value = [];
+  };
+
+  const checked = ref(false);
+
   const toggleChecked = () => {
-    checked.value = !checked.value
-  }
+    checked.value = !checked.value;
+  };
 
-  return { products, allProducts, totalSum, totalCount, incrementCount, decrementCount, toggleChecked, checked }
-})
+  return {
+    products,
+    allProducts,
+    totalSum,
+    totalCount,
+    incrementCount,
+    decrementCount,
+    toggleChecked,
+    checked,
+    delAllProducts,
+  };
+});
