fbf846ece5c97433c038f5ca0f5f6e98d05319df
serj2626 <serj2626@mail.ru>
2025-01-20 21:25:21 +0300

    style: del other code

--- components/ShoppingCart/index.vue
+++ components/ShoppingCart/index.vue
@@ -7,33 +7,6 @@ const { allProducts: products } = storeToRefs(store);
 </script>
 
 <template>
-  <!-- <div class="shopping-cart shooping-cart_margin">
-    <div class="container shooping-cart__container">
-      <div class="shooping-cart__top">
-        <div class="shooping-cart__content">
-          <h2 class="shooping-cart__title">Ваша корзина</h2>
-          <span class="shooping-cart__count">4 товара</span>
-        </div>
-
-        <a class="shooping-cart__clear">Очистить корзину</a>
-      </div>
-
-      <div class="shooping-cart__main">
-        <div class="shopping-cart__list-products">
-          <ShoppingCartProduct />
-          <ShoppingCartProduct />
-          <ShoppingCartProduct />
-        </div>
-        <ShoppingCartTotal />
-      </div>
-      <div class="shooping-cart__bottom">
-        <div class="shooping-cart__bottom-notice">
-dsa
-        </div>
-      </div>
-    </div>
-  </div> -->
-
   <div class="shopping-cart shooping-cart_margin">
     <div class="container shooping-cart__container">
       <div class="shooping-cart__top">
