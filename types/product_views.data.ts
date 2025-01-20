b7e395da048197ca9fc8cfac352b16eef5f43cdc
serj2626 <serj2626@mail.ru>
2025-01-20 03:59:51 +0300

    feat: add data all views products

---
+++ types/product_views.data.ts
@@ -0,0 +1,39 @@
+export interface IProduct {
+    title: string,
+    description: string,
+    image: string,
+    priceRub: string
+    priceEur: string
+}
+
+
+export const productViews: IProduct[] = [
+    {
+        title: "BXC",
+        description: "Вытяжное устройство для механической системы вентиляции",
+        image: "~/assets/img/products/zero.jpg",
+        priceRub: "6 848 ₽ – 56 584 ₽",
+        priceEur: "77.60 € – 643.86 €"
+    },
+    {
+        title: "G2H",
+        description: "Многофункциональное вытяжное устройство для естественной и гибридной вентиляции",
+        image: "~/assets/img/products/one.jpg",
+        priceRub: "6 848 ₽ – 56 584 ₽",
+        priceEur: "77.60 € – 643.86 €"
+    },
+    {
+        title: "GHN",
+        description: "Вытяжное устройство с датчиком присутствия",
+        image: "~/assets/img/products/two.jpg",
+        priceRub: "6 848 ₽ – 56 584 ₽",
+        priceEur: "77.60 € – 643.86 €"
+    },
+    {
+        title: "TDA",
+        description: "Вытяжное устройство с датчиком присутствия",
+        image: "~/assets/img/products/three.jpg",
+        priceRub: "6 848 ₽ – 56 584 ₽",
+        priceEur: "77.60 € – 643.86 €"
+    }
+]
