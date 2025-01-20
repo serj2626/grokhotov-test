dcb979f0cc628369accabeae75905659bc188b52
serj2626 <serj2626@mail.ru>
2025-01-20 18:52:57 +0300

    feat: add loop for by products

--- components/ShoppingCart/index.vue
+++ components/ShoppingCart/index.vue
@@ -1,7 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useProductsStore } from "~/store/products";
+import { storeToRefs } from "pinia";
+
+const store = useProductsStore();
+const { products } = storeToRefs(store);
+</script>
 
 <template>
-  <div class="shopping-cart shooping-cart_margin">
+  <!-- <div class="shopping-cart shooping-cart_margin">
     <div class="container shooping-cart__container">
       <div class="shooping-cart__top">
         <div class="shooping-cart__content">
@@ -26,6 +32,33 @@ dsa
         </div>
       </div>
     </div>
+  </div> -->
+
+  <div class="shopping-cart shooping-cart_margin">
+    <div class="container shooping-cart__container">
+      <div class="shooping-cart__top">
+        <div class="shooping-cart__content">
+          <h2 class="shooping-cart__title">Ваша корзина</h2>
+          <span class="shooping-cart__count">4 товара</span>
+        </div>
+
+        <a class="shooping-cart__clear">Очистить корзину</a>
+      </div>
+
+      <div class="shooping-cart__main">
+        <div class="shopping-cart__list-products">
+          <ShoppingCartProduct
+            v-for="(product, index) in products"
+            :key="index"
+            :product="product"
+          />
+        </div>
+        <ShoppingCartTotal />
+      </div>
+      <div class="shooping-cart__bottom">
+        <Notice class="shooping-cart__notice" />
+      </div>
+    </div>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -35,5 +68,7 @@ dsa
   gap: 25px;
 }
 
-
+.shooping-cart_margin {
+  margin-bottom: 95px;
+}
 </style>
