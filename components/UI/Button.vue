66217c28fe4afc2ec4b3acfde5b89a7c0c897e4e
serj2626 <serj2626@mail.ru>
2025-01-20 14:58:38 +0300

    feat: added dynamic styles

--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -1,25 +1,32 @@
 <script lang="ts" setup>
 interface IProps {
-  type: "button" | "submit" | "reset";
   color: "blue" | "white";
-  size: string;
+  size?: string;
 }
+const props = defineProps<IProps>();
+
+const styleObject = computed(() => {
+  return {
+    backgroundColor: props.color === "blue" ? "#0069b4" : "white",
+    color: props.color === "blue" ? "white" : "#0069b4",
+    outline: props.color === "blue" ? "none" : "1px solid #0069b4",
+  };
+});
 </script>
 
 <template>
-  <button class="btn"><slot /></button>
+  <button class="btn" :style="styleObject"><slot /></button>
 </template>
 
 <style lang="scss" scoped>
 .btn {
   display: block;
+  box-sizing: border-box;
   border-radius: 4px;
-  font-size: 16px;
-  color: white;
-  background-color: #0069b4;
-  padding-block: 15px;
+  font-size: 18px;
+  padding-block: 14px;
   border: none;
   width: 100%;
-  margin-top: 20px;
+  transition: var(--transition-base);
 }
 </style>
