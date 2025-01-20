5f9c1f08f47c91e4810ad7c9891792270d92cbcb
serj2626 <serj2626@mail.ru>
2025-01-20 22:22:21 +0300

    refactor: add module swiper

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@pinia/nuxt',
-
-  ],
+  modules: ['@pinia/nuxt', 'nuxt-swiper'],
   css: ['~/assets/styles/main.scss'],
   app: {
     rootId: "app",
