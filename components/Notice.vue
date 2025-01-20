d8ed214b460d1a08dba6d74c0e9d32bfb62f4046
serj2626 <serj2626@mail.ru>
2025-01-20 14:57:11 +0300

    feat: add code

---
+++ components/Notice.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="notice">
+    <input class="notice__checkbox" type="checkbox" />
+    <img
+      class="notice__img"
+      src="~/assets/img/installed.svg"
+      alt="Install"
+      loading="lazy"
+    />
+    <div class="notice__info">
+      <p class="notice__title">Установка</p>
+      <span class="notice__desc">
+        Отметьте, если Вам необходима консультация профессионала по монтажу выбранных товаров.
+      </span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.notice {
+  width: 800px;
+  display: flex;
+  align-items: center;
+  padding-block: 29px;
+  padding-right: 42px;
+  padding-left: 25px;
+  background-color: var(--color-gray);
+}
+.notice__checkbox {
+  width: 20px;
+  height: 20px;
+}
+.notice__img {
+  margin-left: 20px;
+}
+.notice__info {
+  margin-left: 20px;
+}
+.notice__title {
+  color: var(--color-dark);
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 145%;
+}
+.notice__desc {
+  font-size: 14px;
+  line-height: 150%;
+  color: var(--color-article-price);
+}
+</style>
