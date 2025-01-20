0a81c2462c09a4bb5735ed992ad32245df9fb8cf
serj2626 <serj2626@mail.ru>
2025-01-20 18:56:36 +0300

    feat: add import store and set values by totalSum and totalCount

--- components/ShoppingCart/Total.vue
+++ components/ShoppingCart/Total.vue
@@ -1,4 +1,11 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { useProductsStore } from "~/store/products";
+import { storeToRefs } from "pinia";
+
+const store = useProductsStore();
+
+const { totalCount, totalSum } = storeToRefs(store);
+</script>
 
 <template>
   <section class="total">
@@ -7,11 +14,11 @@
       <div class="total__top-content">
         <div class="total__sum">
           <p class="total__sum-title">Сумма заказа</p>
-          <p class="total__sum-value">50 576 ₽</p>
+          <p class="total__sum-value">{{ totalSum }} ₽</p>
         </div>
         <div class="total__count">
           <p class="total__count-title">Количество</p>
-          <p class="total__count-value">4 шт</p>
+          <p class="total__count-value">{{ totalCount }} шт</p>
         </div>
         <div class="total__installation">
           <p class="total__installation-title">Установка</p>
@@ -23,23 +30,20 @@
     <div class="total__footer">
       <div class="total__footer-content">
         <p class="total__footer-title">Стоимость товаров</p>
-        <p class="total__footer-value">50 576 ₽</p>
+        <p class="total__footer-value">{{ totalSum }} ₽</p>
       </div>
 
       <div class="total__actions">
-        <UIButton :color="'blue'" class="total__btn"
-          >Оформить заказ</UIButton
-        >
+        <UIButton :color="'blue'" class="total__btn">Оформить заказ</UIButton>
         <UIButton :color="'white'" class="total__btn">Купить в 1 клик</UIButton>
       </div>
     </div>
   </section>
 </template>
 <style lang="scss" scoped>
-.total__actions{
+.total__actions {
   display: flex;
   flex-direction: column;
   gap: 11px;
 }
-
 </style>
