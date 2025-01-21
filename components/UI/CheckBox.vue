c42069625ef9e63cc481d03ea87c90c0683184fc
serj2626 <serj2626@mail.ru>
2025-01-21 04:09:26 +0300

    feat: new ui component by checkbox

--- components/UI/CheckBox.vue
+++ components/UI/CheckBox.vue
@@ -1,11 +1,21 @@
 <script lang="ts" setup>
- 
+defineProps<{
+  checked: boolean;
+}>();
 </script>
 
 <template>
- 
+  <input
+    class="notice__checkbox"
+    :checked="checked"
+    @click="$emit('toggle')"
+    type="checkbox"
+  />
 </template>
 
 <style lang="scss" scoped>
- 
+.notice__checkbox {
+  width: 20px;
+  height: 20px;
+}
 </style>
