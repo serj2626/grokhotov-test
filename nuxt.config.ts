b101190976b8120a6df0fd7216ab0eee9438ba61
serj2626 <serj2626@mail.ru>
2025-01-21 06:19:25 +0300

    feat: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', 'nuxt-swiper'],
-  css: ['~/assets/styles/main.scss'],
+  modules: ["@pinia/nuxt", "nuxt-swiper"],
+  css: ["~/assets/styles/main.scss"],
   app: {
     rootId: "app",
     head: {
-      title: 'Grokhotov Test',
-    }
+      title: "Grokhotov Test",
+    },
   },
-})
+});
